a7911d3339fe97d1c1e2ee796aab21bc6cde68ce
n1mnot1more <johmey2023@gmail.com>
2026-05-29 12:10:10 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import adapterStatic from "@sveltejs/adapter-static";
 import { sveltePreprocess } from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
+const dev = process.argv.includes('dev');
+
 const preprocess = sveltePreprocess({
 	postcss: {
 		plugins: [autoprefixer]
